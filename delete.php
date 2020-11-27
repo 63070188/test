444c439e036f2ad6fb889579a3530e09bad2672c
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-27 12:24:45 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,15 @@
 <?php
-mysqli_real_connect($conn, 'labit.mysql.database.azure.com', 'aphatsara836@labit', 'Po0926245419', 'ITFLab', 3306);
-$id=$_GET['id'];
-$sql="DELETE FROM guestbook WHERE id='$id'";
-if (mysqli_query($conn, $sql)) {
-    echo "Delete successfully";
-    echo "<a href="index.php">back to home page</a>";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-
+    mysqli_real_connect($conn, 'labit.mysql.database.azure.com', 'aphatsara836@labit', 'Po0926245419', 'ITFLab', 3306);
+    $id=$_GET['id'];
+    $sql="DELETE FROM guestbook WHERE id='$id'";
+<!DOCTYPE html>
+<html>
 ?>
+    if (mysqli_query($conn, $sql)) {
+        echo "Delete successfully";
+        echo "<a href="index.php">back to home page</a>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+</html>
+
