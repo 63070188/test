ba21f51dca709c1022e6e4040f21b2c864a2a806
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-27 12:27:52 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,11 +5,12 @@
 <!DOCTYPE html>
 <html>
 ?>
-    if (mysqli_query($conn, $sql)) {
+   <?php if (mysqli_query($conn, $sql)) {
         echo "Delete successfully";
         echo "<a href="index.php">back to home page</a>";
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
      }
+    ?>
 </html>
 
