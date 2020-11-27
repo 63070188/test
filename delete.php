d44f657605ddbd0aae5b9cb686cfbae1502fea7d
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-27 12:19:02 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-require('connect.php');
+mysqli_real_connect($conn, 'labit.mysql.database.azure.com', 'aphatsara836@labit', 'Po0926245419', 'ITFLab', 3306);
 $id=$_GET['id'];
 $sql="DELETE FROM guestbook WHERE id='$id'";
 if (mysqli_query($conn, $sql)) {
