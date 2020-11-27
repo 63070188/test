eef67f84929e701b64fd4586e44d48dab4a779fe
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-27 15:36:50 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,41 +1,37 @@
-<?php
-	$conn = mysqli_connect('labit.mysql.database.azure.com', 'aphatsara836@labit', 'Po0926245419', 'ITFLab');
-
-	$product = $_POST['product'];
-	$price = $_POST['price'];
-	$discount = $_POST['discount'];
-	$ID = $_POST['ID'];
-
-    $sql = 'UPDATE guestboook SET Product = "'.$product.'", Price = "'.$price.'", Discount = "'.$discount.'" WHERE ID = '.$ID.'';
-    
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Update</title>
+    <title>ITF Lab</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
-    <div class="container mt-5 text-center">
-      
-            <h3>
-                <?php
-                    if (mysqli_query($conn, $sql)) {
-                        echo "Update Completed";
-                    } else {
-                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                    }
-                    mysqli_close($conn);
-                ?>
-            </h3>
-            <a href="index.php" class="btn btn-light mt-3">BACK</a>
-       
-    </div>
-</body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'labit.mysql.database.azure.com', 'aphatsara836@labit', 'Po0926245419', 'ITFlab', 3306);
+if (!$conn)
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 
+$ID = $_POST['ID'];
+$product = $_POST['product'];
+$price = $_POST['price'];
+$discount = $_POST['discount'];
+$sql = "UPDATE guestbook SET Product='$product', Price='$price', Discount='$discount' WHERE ID='$ID'";
+
+if (mysqli_query($conn, $sql)) {
+    echo '<div class="container">
+            <h3>Edited successfully.</h3>
+            <a role="button" class="btn btn-primary mt-3" href="index.php">Home</a>
+         </div>';
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+</body>
 </html>
